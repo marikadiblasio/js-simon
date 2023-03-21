976a09d89f3b695dcbb0c943c02c57936cf648ed
marikadiblasio <marbla.albram@gmail.com>
2023-03-21 17:53:47 +0100

    add form and check of inputs with results

--- js/script.js
+++ js/script.js
@@ -26,7 +26,6 @@ function generateNum(){
     }
     return numbers //ritorno l'array pieno
 }
-
 function play(){
     //Numeri casuali
     const pcNumbers = document.getElementById('pcNumbers'); //prendo il div
@@ -37,15 +36,42 @@ function play(){
     let seconds = secondsBox.innerText; //selezioni il contenuto
     const countdown = setInterval(() => { //avvio la timing function (ogni secondo)
         seconds--; //decremento i secondi
-       secondsBox.innerText = seconds; //stampo a schermo i secondi
         if (seconds === 0){ //arrivati a 0 
             clearInterval(countdown); //blocco il contdown
             let message = document.querySelector('h2');//prendo l'h2
             message.innerText= `Inserisci i numeri che hai visto`;//cambio il contenuto dell'elemento
             pcNumbers.classList.add('d-none');//nascondo i numeri
-        } 
+            btn.classList.remove('d-none');//rendo visibile il form i numeri
+        } else {
+       secondsBox.innerText = seconds; //stampo a schermo i secondi
+        }
     }, 1000);
-
-}
+//Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+/**
+ * Prendo il bottone
+ * attacco eventlistener
+ * prendo score
+ * prendo i numeri inseriti se numbers.includes(numeri inseriti) incremento score e stampa a schermo score e numeri giusti - stesso h3?)
+ */
+    const btn = document.getElementById('userNumbers');//prendo bottone invia
+    
+    btn.addEventListener('submit', function(e){
+        e.preventDefault();//
+        const userNumsBox = document.querySelectorAll('input[type=number]'); //prendo array di input
+        let score = 0;
+        let rightNums = [];
+        let result = document.querySelector('h3');
+        for (let x of userNumsBox.values()) {//prendo il valore di ogni nodo della lista
+            if (numbers.includes(parseInt(x.value))) { //prendo il valore di ogni input della lista
+                score++; //aumento il punteggio
+                rightNums.push(x.value);//inserisco i numeri nella variabile
+            }  
+        }
+        if(score === 0){
+            result.innerText =`Mi dispiace, non hai ricordato alcun numero!`
+        }else{
+        result.innerText += `Hai ricordato i seguenti numeri: ${rightNums}. In totale hai ricordato ${score} numeri`;
+        }
+    })}
 const start = document.getElementById('start');
 start.addEventListener('click', play);
